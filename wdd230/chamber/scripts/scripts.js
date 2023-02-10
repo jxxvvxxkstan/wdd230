ecdd039df5b6b656b098349a957702791add476a
agustdream <gabriela.guterres@gmail.com>
2023-02-10 16:38:52 -0300

    week4 form

--- wdd230/chamber/scripts/scripts.js
+++ wdd230/chamber/scripts/scripts.js
@@ -55,3 +55,14 @@ if('IntersectionObserver' in window) {
       loadImages(img);
     });
   }
+
+// form
+const rating = document.getElementById("rating");
+const rangevalue = document.getElementById("r");
+
+function displayRatingValue() {
+    rating.innerHTML = rangevalue.value;
+}
+
+rangevalue.addEventListener('change', displayRatingValue);
+rangevalue.addEventListener('input', displayRatingValue);
