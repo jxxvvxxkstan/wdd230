e1df7d505b41331b6fc1cc08f1d38eabe25bec6e
agustdream <gabriela.guterres@gmail.com>
2023-02-18 03:04:37 -0300

    week 6 banner

--- wdd230/chamber/scripts/scripts.js
+++ wdd230/chamber/scripts/scripts.js
@@ -103,5 +103,21 @@ async function apiFetch() {
   
   apiFetch();
 
-//Directory
+// Banner
+
+let d = new Date().getDay();
+
+const banner = document.getElementById("banner");
+if (d === 1 || d === 4) {
+  banner.style.display = "block";
+}
+else {
+    banner.style.display = "none";
+}
+
+const closeBtn = document.getElementById("close");
+
+closeBtn.addEventListener ("click", () => {
+  banner.style.display = "none";
+});
 
